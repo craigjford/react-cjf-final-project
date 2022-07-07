52aa70dc3f8ea3fa145ac348fae509b6c30aaa26
Craig Ford <craig_ford@icloud.com>
2022-07-07 14:04:31 -0400

    added NavLink to NavBar'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return (
-    <h1>Navigation Page</h1>
-  )
-}
+const linkStyles = {
+    display: "inline-block",
+    width: "80px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "green",
+    textDecoration: "none",
+    color: "white",
+  };
 
-export default NavBar
+  function NavBar() {
+    return (
+      <div className="navbar">
+        <NavLink
+          to="/"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkgreen",
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/courseform"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkgreen",
+          }}
+        >
+          CourseForm
+        </NavLink>
+        <NavLink
+          to="/courselist"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkgreen",
+          }}
+        >
+          CourseList
+        </NavLink>
+      </div>
+    )
+  }
+  
+export default NavBar;
