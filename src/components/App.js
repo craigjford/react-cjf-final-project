dfdc5b8e83490e3baa3c165b5d02469e783ce646
Craig Ford <craig_ford@icloud.com>
2022-07-07 13:36:41 -0400

    Added Switch and Route to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import NavBar from './NavBar'
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import NavBar from './NavBar';
 import CourseList from './CourseList';
+import CourseForm from "./CourseForm";
+
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <h1>Home Page</h1>
-      <CourseList />
+        <NavBar /> 
+        <Switch>
+           <Route exact path="/courseform">
+              <CourseForm />
+           </Route>
+           <Route exact path="/courselist">
+              <CourseList />
+           </Route>
+           <Route exact path="/">
+              <Home />
+           </Route>
+        </Switch>
     </div>
   )
 }
 
 export default App;
+
