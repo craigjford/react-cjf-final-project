4e2c2e5c59a67e4cfb6057611c23db9051dd2e86
Craig Ford <craig_ford@icloud.com>
2022-07-09 19:51:28 -0400

    Finished CSS for Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,13 @@ function Home() {
       <hr></hr>
       <article>
         <h3>The Golf Course Application</h3>
-        <p>This application is a list of golf courses. You are welcome to add 
-          golf courses.  Additionally, you can view the list of golf courses
-          by going to CourseList.  When you land on this page, you can Delete a 
-          golf course as well as Like a golf course.  We hope you enjoy the site.
-        </p>
+        <section className={"section-para"}>
+          <p>This application is a list of golf courses. You are welcome to add 
+            golf courses.  Additionally, you can view the list of golf courses
+            by going to CourseList.  When you land on this page, you can Delete a 
+            golf course as well as Like a golf course.  We hope you enjoy the site.
+          </p>
+        </section>
       </article>
     </div>
   )
