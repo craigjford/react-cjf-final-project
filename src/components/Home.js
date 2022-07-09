bc527ba175e699291aea34c9b09684b661b9e223
Craig Ford <craig_ford@icloud.com>
2022-07-09 16:07:14 -0400

    Added description to the Hom Page as well as CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Home() {
   return (
-    <h1>Home Page</h1>
+    <div>
+      <h1 className="homehdr">Home Page</h1>
+      <hr></hr>
+      <article>
+        <h3>The Golf Course Application</h3>
+        <p>This application is a list of golf courses. You are welcome to add 
+          golf courses.  Additionally, you can view the list of golf courses
+          by going to CourseList.  When you land on this page, you can Delete a 
+          golf course as well as Like a golf course.  We hope you enjoy the site.
+        </p>
+      </article>
+    </div>
   )
 }
 
